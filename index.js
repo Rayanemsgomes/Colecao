3db4474dd9d9fce8e71216a58b98947fa717cd10
Rayane Gomes <141366865+Rayanemsgomes@users.noreply.github.com>
2023-10-03 23:06:40 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+const colecao = [
+    {
+        artista: 'Caetano Veloso',
+        valor: 18.50
+    },
+    {
+        artista: 'Legião Urbana',
+        valor: 30
+    },
+    {
+        artista: 'Maria Bethânia',
+        valor: 21.50
+    }
+];
